e6aa0befbe3d32edaf3d9aef838d2cf1254d5ada
Elva Su <112391401+HWenSu@users.noreply.github.com>
2025-06-19 13:17:34 +0800

    Create: APIConnection.js in src folder

---
+++ src/APIConnection.js
@@ -0,0 +1,10 @@
+
+const API_BASE_URL = "http://10.2.3.33:8080/colab/api/product";
+
+export const apiUrls = {
+  api_url_path_g: `${API_BASE_URL}/garment`,
+  api_url_path_f: `${API_BASE_URL}/fabric`,
+  api_url_path_pe: `${API_BASE_URL}/petech`,
+};
+
+
